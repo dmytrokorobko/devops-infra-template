017054eca49e5a8c5eb188633c4e3467fd2d1863
Dmytro Korobko <dmytro.korobko@gmail.com>
2025-12-21 12:35:24 -0500

    Fix syntaxis

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = var.region
 
   default_tags {   
-    tags = var.default-tags
+    tags = var.default_tags
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,4 @@
-variable "default-tags" {
+variable "default_tags" {
   description = "Default tags to apply to all resources"
   type        = map(string)
   default     = {
